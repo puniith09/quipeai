c5ffc3576d4e59c436a3daada466f427055dd425
puniith <puniith09@gmail.com>
2025-10-12 01:51:09 +0530

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+// @ts-ignore - next-pwa doesn't have TypeScript definitions
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
