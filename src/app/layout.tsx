0f00e82fee61964ebdb22cd862ab449a14905ad6
puniith <puniith09@gmail.com>
2025-10-14 00:35:47 +0530

    pwa optimised

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,9 +44,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-      </head>
       <body
         className={`${poppins.variable} antialiased`}
       >
