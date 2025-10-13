5a60b8506b97917ea418a01cbcc52d22247501cc
puniith <puniith09@gmail.com>
2025-10-14 00:53:23 +0530

    pwa optimised with htheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#2563eb",
+  themeColor: "#ffffff",
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
